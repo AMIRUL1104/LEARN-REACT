8bc7075642d9fc23d07cc7085767ef4f419e94e7
Amirul <amirulislam9.e@gmail.com>
2025-11-14 12:09:07 +0600

    secund commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <h1 className='header text-5xl font-bold' >first react app</h1>
+  <p className='bg-slate-900, caret-amber-50 bg-red-500 text-teal-500'> amirul islam</p>
+    </>
+  )
+}
+
+export default App

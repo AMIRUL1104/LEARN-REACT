725ce9e62d3f230f6b7ef2944f86ce7792f0be47
Amirul <amirulislam9.e@gmail.com>
2026-01-26 15:14:52 +0600

    practiceTwo.jsx updated

--- useReducer/src/PracticeTwo.jsx
+++ useReducer/src/PracticeTwo.jsx
@@ -1,24 +1,27 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 const initialState = { count: 1, step: 1 };
 
 function reducer(state, action) {
-  console.log(state.count);
-  console.log(state.step);
-
   switch (action.type) {
     case "INCREMENT":
       return {
         ...state,
-        count: state.count + Number(action.step),
-        step: state.step,
+        count: state.count + state.step,
       };
 
     case "DECREMENT":
+      console.log(state.step);
       return {
         ...state,
-        count: state.count - Number(action.step),
-        step: state.step,
+        count: state.count - state.step,
+      };
+
+    case "setStep":
+      console.log(state.step);
+      return {
+        ...state,
+        step: action.step,
       };
     default:
       return state;
@@ -27,8 +30,6 @@ function reducer(state, action) {
 
 function PracticeTwo() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [step, setStep] = useState(1);
-  console.log(step);
 
   return (
     <div className=" practiceBox">
@@ -40,8 +41,9 @@ function PracticeTwo() {
           type="number"
           name=""
           id="step"
-          onChange={(e) => setStep(e.target.value)}
-          value={step}
+          onChange={(e) =>
+            dispatch({ type: "setStep", step: Number(e.target.value) })
+          }
         />
       </div>
 
@@ -52,7 +54,7 @@ function PracticeTwo() {
           onClick={() =>
             dispatch({
               type: "INCREMENT",
-              step: step,
+              step: state.step,
             })
           }
         >
@@ -64,7 +66,7 @@ function PracticeTwo() {
           onClick={() =>
             dispatch({
               type: "DECREMENT",
-              step: step,
+              step: state.step,
             })
           }
         >
@@ -76,3 +78,82 @@ function PracticeTwo() {
 }
 
 export default PracticeTwo;
+
+// import { useReducer, useState } from "react";
+
+// const initialState = { count: 1, step: 1 };
+
+// function reducer(state, action) {
+//   console.log(state.count);
+//   console.log(state.step);
+
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return {
+//         ...state,
+//         count: state.count + Number(action.step),
+//         step: state.step,
+//       };
+
+//     case "DECREMENT":
+//       return {
+//         ...state,
+//         count: state.count - Number(action.step),
+//         step: state.step,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// function PracticeTwo() {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   const [step, setStep] = useState(1);
+//   console.log(step);
+
+//   return (
+//     <div className=" practiceBox">
+//       <h1>PracticeTwo</h1>
+
+//       <div>
+//         <label htmlFor="step">Step</label>{" "}
+//         <input
+//           type="number"
+//           name=""
+//           id="step"
+//           onChange={(e) => setStep(e.target.value)}
+//           value={step}
+//         />
+//       </div>
+
+//       <h2>{state.count}</h2>
+//       <div>
+//         <button
+//           type="button"
+//           onClick={() =>
+//             dispatch({
+//               type: "INCREMENT",
+//               step: step,
+//             })
+//           }
+//         >
+//           +
+//         </button>
+
+//         <button
+//           type="button"
+//           onClick={() =>
+//             dispatch({
+//               type: "DECREMENT",
+//               step: step,
+//             })
+//           }
+//         >
+//           -
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default PracticeTwo;
